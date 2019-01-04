f8831ad77f3b16972a528adad0f37881613edf59
Administrator <>
2019-01-04 14:58:02 +0800

    1..提交，提交，好开心啊！！！！！！！！！！！！！！！

---
+++ L46数据分析和图表可视化/4京东评论分析图形化展示.py
@@ -0,0 +1,40 @@
+# 综合项目：京东评论情感化分析
+
+import pymysql
+import pymysql
+from wordcloud import WordCloud
+
+# 1 从数据库把所有用户评论查出
+def get_comments():
+    # 连接数据库
+    # 读表
+    # select content from
+    return [{}, {}]
+
+def process_comments():
+    # 所有用户评论拼成一个长字符串
+    return ''
+
+def cut_word(string):
+    # 分词 ，返回wordcloud包使用的格式
+    string
+    ['手机', '好', '质量', '手机']
+
+    return '手机 好 质量 手机'
+
+def word_cloud(string):
+    # 生成词云，保存到本地
+    pass
+    return None
+
+def gen_pei():
+    # 生成饼状图
+    # select  count()  group by
+    # 本地生成饼状图
+    pass
+
+# 1 查所有用户评论内容
+# 2 所有用户评论拼成一个长字符串
+# 3 长字符串用jieba分词
+# 4 拼成wordcloud使用的结构，生成评论高频关键字词云。
+# 5 pygal 根据用户productcolor 统计比例，查询pygal 饼状图文档。
