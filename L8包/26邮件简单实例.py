c2ca8c4117c3e537fecea57ab7f2a940c4fd0897
Administrator <>
2019-01-09 16:51:05 +0800

    6.更新。o(*￣︶￣*)o，，记住，记住！！！！！！！！！！！！！！！

---
+++ L8包/26邮件简单实例.py
@@ -0,0 +1,12 @@
+import yagmail
+import os
+
+sender = '3342899060@qq.com'
+password = 'tuhtbcyhpxgscicc'
+target = '1770323960@qq.com'
+
+yag = yagmail.SMTP(user=sender, password=password, host='smtp.qq.com', port=465, smtp_ssl=True)
+contents = '测试test'
+yag.send(to=target, subject='测试yagmail发邮件', contents=contents)
+print('已发送')
+
