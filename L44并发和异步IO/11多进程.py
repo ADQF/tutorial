88ef8348bf7d046a7d7fb8a82643b039bb29617d
Administrator <>
2019-01-11 10:50:42 +0800

    多线程？？多进程？？？？？？

--- L44并发和异步IO/11多进程.py
+++ L44并发和异步IO/11多进程.py
@@ -1,22 +1,41 @@
+# 多进程
 from multiprocessing import Process
 import time
 import os
 
-
 def info(title):
-    time.sleep(0)
     print(title)
-    print('墨客冥 ', __name__)
-    print('福建城', os.getppid())
-    print('自己进城', os.getppid())
+    print('模块名 ', __name__)
+    print('父进程', os.getppid())
+    print('自己进程', os.getpid())
     print('\n\n')
 
 def f(name):
-    info('紫禁城')
+    info('子进程')
     print('hello', name)
 
 if __name__ == '__main__':
-    info('珠江城')
-    p = Process(target=f, args=('bbb', ))
+    info('主进程')
+    p = Process(target=f, args=('bbb',))
     p.start()
     p.join()
+
+"""
+运行结果
+主进程
+模块名  __main__
+父进程 11984
+自己进程 4900
+
+
+
+子进程
+模块名  __mp_main__
+父进程 4900
+自己进程 2268
+
+
+hello bbb
+"""
+
+# 每个进程windows会分配唯一句柄，叫做pid。每个子进程是从父进程复制而来的。
--- L44并发和异步IO/21生成器.py
+++ L44并发和异步IO/21生成器.py
@@ -3,7 +3,12 @@
 实际上平时一直在使用生成器
 例如 range() jieba.cut()
 
-语法特点：yield生成项：next(生成器对象)  得到一次返回值，每次生成的值会保存到生成器对象中，下一次调用next时
+语法特点：yield生成项：next(生成器对象)  得到一次返回值，每次生成的值会保存到生成器对象中，下一次调用next
 """
 
+def c(n):
+    print('c..')
+    while n < 4:
+        yield n
+        print('increment', n)
 
